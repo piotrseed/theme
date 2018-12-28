d21c84243eb02e6b12ca95392bb58a0e30036e34
PiotrSedek <piotrseed@gmail.com>
2018-12-28 11:49:30 +0100

    poprawki

--- front-page.php
+++ front-page.php
@@ -13,4 +13,97 @@ get_header();
   </div>
 </section>
 
+
+<section class="sectionNazwa">
+  <div class="wrapp">
+    <div class="seditBlock">
+      <div class="block"></div>
+      <div class="block"></div>
+      <div class="block"></div>
+    </div>
+  </div>
+</section>
+
+
+<section class="sectionNazwa">
+  <div class="wrapp">
+    <div class="seditBlock">
+      <div class="block"></div>
+      <div class="block"></div>
+      <div class="block"></div>
+    </div>
+  </div>
+</section>
+
+
+<section class="sectionNazwa">
+  <div class="wrapp">
+    <div class="seditBlock">
+      <div class="block"></div>
+      <div class="block"></div>
+      <div class="block"></div>
+    </div>
+  </div>
+</section>
+
+
+<section class="sectionNazwa">
+  <div class="wrapp">
+    <div class="seditBlock">
+      <div class="block"></div>
+      <div class="block"></div>
+      <div class="block"></div>
+    </div>
+  </div>
+</section>
+
+
+<section class="sectionNazwa">
+  <div class="wrapp">
+    <div class="seditBlock">
+      <div class="block"></div>
+      <div class="block"></div>
+      <div class="block"></div>
+    </div>
+  </div>
+</section>
+
+
+<section class="sectionSkill">
+  <div class="wrapp">
+    <div class="seditBlock">
+      <div class="block">
+        <h1>Zastosowanie</h1>
+        <span></span>
+      </div>
+      <div class="block"></div>
+      <div class="block"></div>
+      <div class="block"></div>
+      <div class="block"></div>
+    </div>
+  </div>
+</section>
+
+
+<section class="sectionNazwa">
+  <div class="wrapp">
+    <div class="seditBlock">
+      <div class="block"></div>
+      <div class="block"></div>
+      <div class="block"></div>
+    </div>
+  </div>
+</section>
+
+
+<section class="sectionNazwa">
+  <div class="wrapp">
+    <div class="seditBlock">
+      <div class="block"></div>
+      <div class="block"></div>
+      <div class="block"></div>
+    </div>
+  </div>
+</section>
+
 <?php get_footer();
